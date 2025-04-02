a30b15cc212eae6cd0c09844ee5ac2b22c956bf6
piyushkrsingh07 <piyush23153101@akgec.ac.in>
2025-04-02 16:34:51 +0530

    handling multiple routes done

--- src/app.js
+++ src/app.js
@@ -13,34 +13,81 @@ const app=express();
 //     res.send("Hello Hello Hello Hello")
  
 // })
-app.use("/user",(req,res)=>{
-    res.send("haaahaha")
-})
+// app.use("/user",(req,res)=>{
+//     res.send("haaahaha")
+// })
 
-app.get("/user",(req,res)=>{
-    res.send({firstName:"Piyush",lastName:"Kumar"})
-})
+//it will match req/user ,/user/xyz,/user/1
+// app.get("/user",(req,res)=>{
+//     res.send({firstName:"Piyush",lastName:"Kumar"})
+// })
 
-app.post("/user",(req,res)=>{
-    console.log("save data to db");
-    res.send("Data successfully saved to the database")
-})
+// app.post("/user",(req,res)=>{
+//     console.log("save data to db");
+//     res.send("Data successfully saved to the database")
+// })
 
-app.delete("/user",(req,res)=>{
-    res.send("Deleted successfully")
-})
+// app.delete("/user",(req,res)=>{
+//     res.send("Deleted successfully")
+// })
 
-app.use('/test',(req,res)=>{
-    res.send("Hello from the server")
-})
+// app.use('/test',(req,res)=>{
+//     res.send("Hello from the server")
+// })
 
 // app.use("/",(req,res)=>{
 //     res.send("namaste from dashboard ")
  
 // })
 
+// app.get("/ab*cd",(req,res)=>{
+//       res.send({firstName:"Piyush",lastName:"Kumar"})
+// })
+// app.get(/.*fly$/,(req,res)=>{
+//       res.send({firstName:"Piyush",lastName:"Singh"})
 
 
-app.listen(7778,()=>{
-    console.log("server is successfully listen on port 7778.....")
+// app.get("/user",(req,res)=>{
+//     console.log(req.query)
+//     res.send({firstName:"Piyush",lastName:"Kumar"})
+// })
+//to handle dynamic route
+
+// app.get("/user/:userId/:name/:password",(req,res)=>{
+//     console.log(req.params)
+//          res.send({firstName:"Piyush",lastName:"Kumar"}) 
+// })
+
+
+app.use("/user",[
+    (req,res,next)=>{
+        console.log("Handling the route user 1")
+      
+
+      next();
+    
+
+},
+(req,res,next)=>{
+    console.log("Handling the route user 2")
+    next()
+ 
+},
+],
+(req,res,next)=>{
+    console.log("Handling the route user 3")
+    next()
+},
+(req,res,next)=>{
+    console.log("Handling the route user 4")
+    res.send("4th Response")
+
+}
+)
+
+
+
+
+app.listen(7777,()=>{
+    console.log("server is successfully listen on port 7777.....")
 })
