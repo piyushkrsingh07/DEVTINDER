30721fc4b24ee179a28b079e60b1e82e052371db
piyushkrsingh07 <piyush23153101@akgec.ac.in>
2025-04-06 18:28:51 +0530

    updated schema and validation

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const connectDB = require('./config/database');
 const app=express();
 
 const User=require("./models/user")
-
+app.use(express.json())
+//this middleware will read the json object convert it into js object and will js object to body
 
 
 
@@ -14,17 +15,13 @@ const User=require("./models/user")
 
 
 app.post("/signup",async(req,res)=>{
-       
+        console.log(req.body)
+
+        const user=new User(req.body)  //create a new instance of user object
+
+
     
 
-    const user=new User(
-        {
-            firstName:"Piyush",
-            lastName:"Kumar",
-            emailId:"piyush02040@gmail.com",
-            password:"piyush@123",
-        }
-    )
 
 
      try{
@@ -35,7 +32,7 @@ app.post("/signup",async(req,res)=>{
    res.send("user added successfully")
      }catch(error){
         // res.status(404).json({error:error.message})
-     res.status(400).send("Error saving the user"+err.message)
+     res.status(400).send("Error saving the user"+error.message)
      }
    
 
@@ -48,6 +45,91 @@ app.post("/signup",async(req,res)=>{
 
 }
 )
+
+// app.get("/feed",(req,res))
+
+//get user by email
+
+app.get("/user",async(req,res)=>{
+    const userEmail=req.body.emailId;
+    
+    try{
+        console.log(userEmail)
+    const user= await User.findOne({emailId:userEmail})
+
+      
+  
+    if(!user){
+        console.log("here it is ")
+       return res.status(404).send("user not found")
+    }
+    
+        res.send(user)
+
+
+   
+    }catch(error){
+        console.error("Error fetching user:", error); // Always log the error
+        res.status(500).send("Something went wrong");
+    }
+
+})
+
+//DELETE THE USER
+app.delete("/user",async(req,res)=>{
+    const userId=req.body.userId
+    try{
+       const user=await User.findByIdAndDelete({_id:userId}) //const user=await user.findByIdAndDelete({_id:userId})
+       if(!user){
+       
+       return res.status(404).send("user not found")
+    }
+    else{
+        res.send("user deleted successfully")
+    }
+       
+    }catch(error){
+    console.error("Error fetching user:", error); // Always log the error
+        res.status(500).send("Something went wrong");
+    }
+
+})
+
+//update by userId
+app.patch("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    
+
+    try{
+    //  const updatedData=await User.findByIdAndUpdate(userId,data,{returnDocument:'before'}) //if anything is not in the database will be ignored by apis
+     const updatedData=await User.findByIdAndUpdate({_id:userId},data,
+        {runValidators:true}
+     )
+     console.log(updatedData)
+  if(!updatedData){
+    res.send("user connot be updated successfully")
+  }
+  else{
+    res.send("user updated successfully")
+  }
+    }catch(error){
+        console.error("Error fetching user:", error); // Always log the error
+        res.status(400).send("UPDATE FAILED:"+ error.message);
+    }
+})
+
+
+app.get("/feed",async(req,res)=>{
+    try{
+const users=await User.find({})
+res.send(users)
+
+
+    }catch(error){
+        res.status(400).send("Somethong went wrong")
+    }
+})
 
 connectDB().then(() => {
     // Start the server only after DB connects
--- src/models/user.js
+++ src/models/user.js
@@ -2,25 +2,54 @@ const mongoose=require('mongoose')
 
 const userSchema=new mongoose.Schema({
    firstName:{
-    type:String
+    type:String,
+    required:true,
+    minLength:4,
+    maxLength:50
    },
    lastName:{
     type:String
    },
    emailId:{
-    type:String
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true,
+    trim:true
    },
    password:{
-    type:String
+    type:String,
+    required:true,
+  
    },
    age:{
-    type:Number
+    type:Number,
+    min:18
    },
+   photoUrl:{
+    type:String,
+    default:"https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg"
+   },
+   about:{
+   type:String,
+   default:"This is the default about of the user"
+   },
+
    gender:{
-    type:String
-   }
+    type:String,
+    validate(value){
+      if(!["male","female","other"].includes(value)){
+         throw new Error("Gender data is not valid")
+      }
+    }
+   },
+   skills:{
+      type:[String]
+   },
 
    
+},{
+   timestamps:true
 })
 
 
